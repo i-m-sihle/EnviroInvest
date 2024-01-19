ea0d06cb8f95c1ae7bdb2dc3a4ac4e79ed120eaf
Sihle Ndlovu <sihlendlovu@pop-os.localdomain>
2024-01-19 21:08:08 +0200

    testing 123

---
+++ src/test/java/com/enviro365/enviroinvests/EnviroinvestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro365.enviroinvests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroinvestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
